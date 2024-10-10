77b073961ac7c653aa370765a452a85d7e34483e
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2024-10-10 22:28:35 +0600

    feat: dark mood

--- components/Layout/header.tsx
+++ components/Layout/header.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
-    return <header></header>;
+    return (
+        <header className="bg-background  lg:pt-[61px] ">
+            <header className="container flex justify-between items-center    ">
+                <Link href={`/`}>
+                    <Image
+                        width={297}
+                        height={67}
+                        src={`/assets/logo.png`}
+                        alt={`logo`}
+                        sizes="100"
+                        className="object-contain"
+                    />
+                </Link>
+            </header>
+        </header>
+    );
 };
 
 export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "selector",
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                text: "var(--text)",
+                primary: "var(--primary)",
+                secondary: "var(--secondary)",
+            },
+            container: {
+                center: true,
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
+
 export default config;
